657af129ebe57d0517d54a8e549147f1c19fe3f0
Shayajs <espinar.lucas0@gmail.com>
2026-01-03 15:24:06 +0100

    Corrections

--- resources/views/entreprise/create.blade.php
+++ resources/views/entreprise/create.blade.php
@@ -294,6 +294,7 @@
                             </p>
                             @error('rayon_deplacement')
                                 <p class="mt-1 text-sm text-red-600 dark:text-red-400">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
 
